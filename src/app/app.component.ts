5de2b9b933dd91f740fad98abd69d5f64b533573
rbasehewa <rbasehewa@gmail.com>
2022-01-16 09:04:06 +1100

    Adding post and put requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from './interface/user';
 import { UserService } from './service/user.service';
 
 @Component({
@@ -9,21 +10,49 @@ import { UserService } from './service/user.service';
 export class AppComponent implements OnInit {
   title = 'AngularHttp';
 
+
+  private user: User = {
+    'id': 1,
+    'name': 'Ryan MMM',
+    'username': 'rbase',
+    'email': 'abc123@april.biz',
+    'address': {
+      'street': 'Kulas Light',
+      'suite': 'Apt. 556',
+      'city': 'Gwenborough',
+      'zipcode': '92998-3874',
+      'geo': {
+        'lat': '-37.3159',
+        'lng': '81.1496'
+      }
+    },
+    'phone': '1-770-736-8031 x56442',
+    'website': 'hildegard.org',
+    'company': {
+      'name': 'Vocus',
+      'catchPhrase': 'Multi-layered client-server neural-net',
+      'bs': 'harness real-time e-markets'
+    }
+  }
+
   constructor(private userService:UserService){}
 
 
   ngOnInit(): void {
+    this.onUpdateUser();
     this.onGetUsers();
-    this.onGetUser();
+    // this.onGetUser();
+    // this.onCreateUser();
+
   } 
 
 
   onGetUsers():void{
 
     this.userService.getUsers().subscribe(
-      (response) => console.log(response),
-      (error: any) => console.log(error),
-      () => console.log('Done getiing users')
+      (response) => console.table(response), //response
+      (error: any) => console.log(error),  //error
+      () => console.log('Done getiing users')  // completion
       );
     }
 
@@ -35,6 +64,24 @@ export class AppComponent implements OnInit {
         () => console.log('Done getiing users')
         );
       }
+
+      onCreateUser():void{
+
+        this.userService.createUser(this.user).subscribe(
+          (response) => console.log(response),
+          (error: any) => console.log(error),
+          () => console.log('Done creating new user')
+          );
+        }
+
+        onUpdateUser():void{
+
+          this.userService.updateUser(this.user).subscribe(
+            (response) => console.log(response),
+            (error: any) => console.log(error),
+            () => console.log('Updated the user')
+            );
+          }
 
 
   }
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -2,20 +2,31 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../interface/user';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+  private apiUrl= environment.apiUrl; // create an environment variable
+
   constructor(private http: HttpClient) { }
 
   getUsers(): Observable<User[]>{
-    return this.http.get<User[]>(`https://jsonplaceholder.typicode.com/users`);
+    return this.http.get<User[]>(`${this.apiUrl}/users`);
   }
 
   getUser(): Observable<User>{
-    return this.http.get<User>(`https://jsonplaceholder.typicode.com/users/1`);
+    return this.http.get<User>(`${this.apiUrl}/users/1`);
+  }
+
+  createUser(user: User): Observable<User>{ // post method
+    return this.http.post<User>(`${this.apiUrl}/users`,user); // pass this user for payload
+  }
+
+  updateUser(user: User): Observable<User>{ // Put method - need to send entire data
+    return this.http.put<User>(`${this.apiUrl}/users/${user.id}`,user); // pass this user for payload
   }
 
 }
