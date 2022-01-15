03c4bd03e883f94f629dcc4a0c50309a107be053
rbasehewa <rbasehewa@gmail.com>
2022-01-15 23:03:53 +1100

    basic example of subscribing to observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './service/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngularHttp';
-}
+
+  constructor(private userService:UserService){}
+
+
+  ngOnInit(): void {
+    this.onGetUsers();
+    this.onGetUser();
+  } 
+
+
+  onGetUsers():void{
+
+    this.userService.getUsers().subscribe(
+      (response) => console.log(response),
+      (error: any) => console.log(error),
+      () => console.log('Done getiing users')
+      );
+    }
+
+    onGetUser():void{
+
+      this.userService.getUser().subscribe(
+        (response) => console.log(response),
+        (error: any) => console.log(error),
+        () => console.log('Done getiing users')
+        );
+      }
+
+
+  }
